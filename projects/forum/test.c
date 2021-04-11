cdc9457449bac5dd932cd5d81df3db3a62d1d8fe
Torsten Hemmecke <Torsten.Hemmecke@web.de>
2021-04-11 17:28:33 +0000

    itoa

---
+++ projects/forum/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <signal.h>
+#include <math.h>
+#include <memory.h>
+#include <conio.h>
+
+//#define NKC_DEBUG	
+
+#ifdef NKC_DEBUG
+#include "../nkc/llnkc.h"
+#endif
+
+
+
+
+
+
+int main(int argc, const char* argv[])
+{
+  int i;
+  float f;
+  char str[11];
+
+  i = 10;
+  itoa (i,str,10);
+}
+
+
+
+
+
